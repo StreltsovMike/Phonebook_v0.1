e70014f968a34daf3adc918d4501d9e9efd7790d
Streltsov_Mike <streltsovmike@yandex.ru>
2022-09-26 18:11:46 +0300

    Created Phonebook version 0.1(demo)

--- controller.py
+++ controller.py
@@ -1,17 +1,17 @@
 import gui
-import dp
+import db
 import comands
 
 def start_phonebook():
     var = comands.start_qгestion()
     if var == 1:
-        dp.add_contact_to_exel(dp.add_contact_to_list(gui.take_contact()))
+        db.add_contact_to_exel(db.add_contact_to_list(gui.take_contact()))
         print('Контакт добавлен в phonebook.xlsx')
     elif var == 2:
-        dp.updated_txt_v1(dp.read_xlsx())
+        db.updated_txt_v1(db.read_xlsx())
         print('Файл с контактами создан - phonebook_v1.txt')
     elif var == 3:
-        dp.updated_txt_v2(dp.read_xlsx())
+        db.updated_txt_v2(db.read_xlsx())
         print('Файл с контактами создан - phonebook_v2.txt')
     elif var == 4:
-        dp.find_name(input('Введите Имя: '))
+        db.find_name(input('Введите Имя: '))
